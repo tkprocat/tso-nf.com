a78c011872a5d7f4a9b3e7e99b8fd1c0098cbd05
Procat <dev@procat.dk>
2014-08-15 19:27:50 +0200

    Added some top 10 pages.

---
+++ app/views/stats/top10byavgdrop.blade.php
@@ -0,0 +1,54 @@
+@extends('...layouts.default')
+
+{{-- Content --}}
+@section('content')
+<div class="panel panel-default">
+    <div class="panel-heading">
+        Top 10 adventures for loot by amount.
+    </div>
+    <div class="panel-body" style="margin-left: 20px">
+        <form class="form-inline" role="form">
+            <div class="form-group ">
+                <label for="loottype_dropchance">Select loot:</label>
+                {{ Form::select('loottype_avgdrop', $loot_types, array('',''), array('style' => 'width: 200px','class' => 'form-control')) }}
+            </div>
+        </form>
+    </div>
+</div>
+<table id="result_avgdrop" class="table table-striped table-bordered">
+    <thead>
+    <tr><th>Adventure</th><th>Average drop</th></tr>
+    </thead>
+    <tbody>
+    </tbody>
+</table>
+
+
+
+<script>
+    $('[name="loottype_avgdrop"]').change(function () {
+        $.ajax({
+            type: 'GET',
+            url: "{{ URL::to('/stats/getTop10BestAdventuresForLootTypeByAvgDrop') }}",
+            data: {
+                type: $('[name="loottype_avgdrop"] option:selected').text()
+            },
+            statusCode: {
+                401: function () {
+                    self.location = "/login";
+                    return false;
+                }
+            },
+            success: function (lootdata) {
+                $('#result_avgdrop tbody').empty();
+
+                $(lootdata).each(function () {
+                    $('#result_avgdrop tbody').append('<tr><td>'+this.name+'</td><td>'+this.avg_drop+'</td></tr>');
+                });
+            },
+            error: function (msg) {
+            }
+        });
+    });
+</script>
+@stop
