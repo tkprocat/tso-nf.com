1aa687398bb36a5730d2859fbedea22809ea70c8
Allan Knudsen <procat@gmail.com>
2015-04-19 13:07:33 +0200

    Fixed to show the username needed for login and not the email.

--- app/views/emails/auth/welcome.blade.php
+++ app/views/emails/auth/welcome.blade.php
@@ -6,7 +6,7 @@
 	<body>
 		<h2>Welcome</h2>
 
-		<p><b>Account:</b> {{{ $email }}}</p>
+		<p><b>Account:</b> {{{ $username }}}</p>
 		<p>To activate your account, <a href="{{ URL::to('users') }}/{{ $userId }}/activate/{{ urlencode($activationCode) }}">click here.</a></p>
 		<p>Or point your browser to this address: <br /> {{ URL::to('users') }}/{{ $userId }}/activate/{{ urlencode($activationCode) }}</p>
 		<p>Thank you, <br />The tso-nf.com admins</p>
