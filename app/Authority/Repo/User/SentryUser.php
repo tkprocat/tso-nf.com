e646b37f6a4ee52445c2a8cee21525298fae4584
Allan Knudsen <procat@gmail.com>
2015-05-08 22:39:29 +0200

    Stole a password generating method from Catalyst\Sentry and replaced it with the one in Authority. Changed password length to 12 when resetting passwords.

--- app/Authority/Repo/User/SentryUser.php
+++ app/Authority/Repo/User/SentryUser.php
@@ -280,7 +280,7 @@ class SentryUser extends RepoAbstract implements UserInterface {
         {
 	        // Find the user
 	        $user = $this->sentry->getUserProvider()->findById($id);
-	        $newPassword = $this->_generatePassword(8,8);
+	        $newPassword = $this->_generatePassword(12);
 
 			// Attempt to reset the user password
 			if ($user->attemptResetPassword($code, $newPassword))
@@ -555,34 +555,29 @@ class SentryUser extends RepoAbstract implements UserInterface {
      * From http://www.phpscribble.com/i4xzZu/Generate-random-passwords-of-given-length-and-strength
      *
      */
-    private function _generatePassword($length=9, $strength=4) {
-        $vowels = 'aeiouy';
-        $consonants = 'bcdfghjklmnpqrstvwxz';
-        if ($strength & 1) {
-               $consonants .= 'BCDFGHJKLMNPQRSTVWXZ';
-        }
-        if ($strength & 2) {
-               $vowels .= "AEIOUY";
-        }
-        if ($strength & 4) {
-               $consonants .= '23456789';
-        }
-        if ($strength & 8) {
-               $consonants .= '@#$%';
+    private function _generatePassword($length=9) {
+        // We'll check if the user has OpenSSL installed with PHP. If they do
+        // we'll use a better method of getting a random string. Otherwise, we'll
+        // fallback to a reasonably reliable method.
+        if (function_exists('openssl_random_pseudo_bytes'))
+        {
+            // We generate twice as many bytes here because we want to ensure we have
+            // enough after we base64 encode it to get the length we need because we
+            // take out the "/", "+", and "=" characters.
+            $bytes = openssl_random_pseudo_bytes($length * 2);
+
+            // We want to stop execution if the key fails because, well, that is bad.
+            if ($bytes === false)
+            {
+                throw new \RuntimeException('Unable to generate random string.');
+            }
+
+            return substr(str_replace(array('/', '+', '='), '', base64_encode($bytes)), 0, $length);
         }
 
-        $password = '';
-        $alt = time() % 2;
-        for ($i = 0; $i < $length; $i++) {
-            if ($alt == 1) {
-                $password .= $consonants[(rand() % strlen($consonants))];
-                $alt = 0;
-            } else {
-                $password .= $vowels[(rand() % strlen($vowels))];
-                $alt = 1;
-            }
-        }
-        return $password;
+        $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        return substr(str_shuffle(str_repeat($pool, 5)), 0, $length);
     }
 
     /**
