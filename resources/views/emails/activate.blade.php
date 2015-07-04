e6a47a0e68b65ee3e7c052ccb177c4a1f86316da
Allan Knudsen <procat@gmail.com>
2015-07-05 01:12:06 +0200

    More minor changes.

--- resources/views/emails/activate.blade.php
+++ resources/views/emails/activate.blade.php
@@ -6,9 +6,10 @@
 <body>
 <h2>{{ trans('auth.welcome') }}</h2>
 
+<p><b>{{ trans('auth.account') }}:</b> {{{ $username }}}</p>
+
 <p><br>{{ trans('auth.welcome2') }}</p>
 
-<p><b>{{ trans('auth.account') }}:</b> {{{ $username }}}</p>
 <p>{{ trans('auth.toActivate') }} <a href="{{ url('auth/activate/'.$code) }}">{{ trans('auth.clickHere') }}.</a></p>
 <p>{{ trans('auth.toActivate2') }} <br /> <a href="{{ url('auth/activate/'.$code) }}">{{ url('auth/activate/'.$code) }}</a></p>
 <p>{{ trans('auth.thankyou') }}<br />{{ trans('auth.admins') }}</p>
