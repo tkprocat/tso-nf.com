2aa4c0e87404014827d4f5f6e9be0d10ab863b40
Allan Knudsen <procat@gmail.com>
2015-07-30 14:39:35 +0200

    Missed some files.

---
+++ app/Console/Commands/GenerateItems.php
@@ -0,0 +1,60 @@
+<?php namespace LootTracker\Console\Commands;
+
+use App;
+use Illuminate\Console\Command;
+use LootTracker\Repositories\Adventure\AdventureLoot;
+use LootTracker\Repositories\Item\Admin\AdminItemInterface;
+use LootTracker\Repositories\Item\Item;
+
+class GenerateItems extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'lt:generate-items';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generates items from old data.';
+
+    /**
+     * @var AdminItemInterface
+     */
+    protected $itemAdminRepo;
+
+
+    /**
+     * Create a new command instance.
+     *
+     */
+    public function __construct()
+    {
+
+        parent::__construct();
+        $this->itemAdminRepo = App::make(AdminItemInterface::class);
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $adventureLootItems = AdventureLoot::all();
+        foreach($adventureLootItems as $adventureLootItem)
+        {
+            $item = Item::where('name', $adventureLootItem->type)->first();
+            //Check if we found anything.
+            if ($item == null)
+                $item = $this->itemAdminRepo->create(['name' => $adventureLootItem->type, 'category' => 'Unknown', 'user_id' => '1']);
+            $adventureLootItem->item_id = $item->id;
+            $adventureLootItem->save();
+        }
+    }
+}
---
+++ database/migrations/2015_07_29_204648_add_item_id_to_adventure_loot.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddItemIdToAdventureLoot extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasColumn('adventure_loot', 'item_id')) {
+            Schema::table('adventure_loot', function ($table) {
+                $table->integer('item_id')->unsigned()->index();
+                $table->foreign('item_id')->references('id')->on('items');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if (Schema::hasColumn('adventure_loot', 'item_id')) {
+            Schema::table('adventure_loot', function ($table) {
+                $table->dropColumn('item_id');
+            });
+        }
+    }
+}
