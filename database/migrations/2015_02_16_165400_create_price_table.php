c10230b6e1e11bfbda3b9c098b5e10f1fb2d47a6
Allan Knudsen <procat@gmail.com>
2015-08-06 09:45:03 +0200

    Changes foreign key

--- database/migrations/2015_02_16_165400_create_price_table.php
+++ database/migrations/2015_02_16_165400_create_price_table.php
@@ -16,9 +16,9 @@ class CreatePriceTable extends Migration {
 		{
 			$table->increments('id');
 			$table->integer('item_id')->unsigned();
-			$table->foreign('item_id')->references('id')->on('items')->onDelete('cascade');
+			$table->foreign('item_id')->references('id')->on('items')->onDelete('cascade')->onUpdate('cascade');
 			$table->integer('user_id')->unsigned();
-			$table->foreign('user_id')->references('id')->on('users');
+			$table->foreign('user_id')->references('id')->on('users')->onDelete('set null')->onUpdate('cascade');
 			$table->double('min_price');
 			$table->double('avg_price');
 			$table->double('max_price');
