a6d87f72e37807fc1ace1a551e58577259e34e4d
Allan Knudsen <procat@gmail.com>
2015-08-07 18:39:54 +0200

    Added Globalize numberFormatter. Fixed issue with changing value in quantity.

--- resources/views/prices/index.blade.php
+++ resources/views/prices/index.blade.php
@@ -42,23 +42,75 @@
         </div>
     </div>
 </div>
+<script src="/js/requirejs/require.js"></script>
 <script>
     var items;
-    $(document).ready( function () {
-        $.get("/prices/getItemsWithPrices", function(data) {
-            items = data;
-            $('#quantity').change();
+    var formatter;
+
+    require.config({
+        paths: {
+            // Globalize dependencies paths.
+            cldr: "/js/cldr",
+
+            // Unicode CLDR JSON data.
+            "cldr-data": "/js/cldr-data",
+
+            // require.js plugin we'll use to fetch CLDR JSON content.
+            json: "/js/requirejs/json",
+
+            // text is json's dependency.
+            text: "/js/requirejs/text",
+
+            // Globalize.
+            globalize: "/js/globalize"
+        }
+    });
+
+    require([
+        "globalize",
+
+        // CLDR content.
+        "json!cldr-data/main/en/numbers.json",
+        "json!cldr-data/supplemental/likelySubtags.json",
+
+        // Extend Globalize with Date and Number modules.
+        "globalize/number"
+    ], function( Globalize, enNumbers, likelySubtags) {
+        Globalize.load(enNumbers);
+        Globalize.load(likelySubtags);
+        Globalize.locale( "en" );
+        formatter = Globalize.numberFormatter({
+            minimumSignificantDigits: 1,
+            maximumSignificantDigits: 3
+        });
+
+        $(document).ready( function () {
+            $.get("/prices/getItemsWithPrices", function(data) {
+                items = data;
+                $('#quantity').change();
+            });
         });
     });
+
+
+    function formatNumber(value) {
+        //Hack since numberFormatter returns NaN on zero.
+        value = parseInt(value);
+
+        if (value == 0)
+            return 0;
+        else
+            return formatter(value);
+    }
 
     $('#quantity').change(function(){
         var itemsTable = $('#items tbody');
         var quantity = $('#quantity').val();
         //Delete old data
-        itemsTable.empty();
         if ($.fn.dataTable.isDataTable('#items')) {
-            itemsTable.DataTable().destroy();
+            $('#items').DataTable().destroy();
         }
+        itemsTable.empty();
         //Repopulate table
         $.each(items, function(index, item) {
             if (quantity == 0) {
@@ -67,37 +119,37 @@
                             '<tr><td><a href="/prices/'+encodeURIComponent(item.name)+'">'+item.name+'</a></td>'+
                             '<td>'+item.category+'</td>' +
                             '<td>1</td>' +
-                            '<td>'+Math.round(item.current_price.min_price*1000)/1000+'</td>' +
-                            '<td>'+Math.round(item.current_price.avg_price*1000)/1000+'</td>' +
-                            '<td>'+Math.round(item.current_price.max_price*1000)/1000+'</td></tr>'
+                            '<td>'+formatNumber(item.current_price.min_price)+'</td>' +
+                            '<td>'+formatNumber(item.current_price.avg_price)+'</td>' +
+                            '<td>'+formatNumber(item.current_price.max_price)+'</td></tr>'
                     )
                 } else if (item.category == 'Buff') {
                     itemsTable.append(
                             '<tr><td><a href="/prices/'+encodeURIComponent(item.name)+'">'+item.name+'</a></td>'+
                             '<td>'+item.category+'</td>' +
                             '<td>25</td>' +
-                            '<td>'+Math.round(item.current_price.min_price*25*1000)/1000+'</td>' +
-                            '<td>'+Math.round(item.current_price.avg_price*25*1000)/1000+'</td>' +
-                            '<td>'+Math.round(item.current_price.max_price*25*1000)/1000+'</td></tr>'
+                            '<td>'+formatNumber(item.current_price.min_price*25)+'</td>' +
+                            '<td>'+formatNumber(item.current_price.avg_price*25)+'</td>' +
+                            '<td>'+formatNumber(item.current_price.max_price*25)+'</td></tr>'
                     )
                 } else {
                     itemsTable.append(
                             '<tr><td><a href="/prices/'+encodeURIComponent(item.name)+'">'+item.name+'</a></td>'+
                             '<td>'+item.category+'</td>' +
-                            '<td>1000</td>' +
-                            '<td>'+Math.round(item.current_price.min_price*1000*1000)/1000+'</td>' +
-                            '<td>'+Math.round(item.current_price.avg_price*1000*1000)/1000+'</td>' +
-                            '<td>'+Math.round(item.current_price.max_price*1000*1000)/1000+'</td></tr>'
+                            '<td>'+formatNumber(1000)+'</td>' +
+                            '<td>'+formatNumber(item.current_price.min_price*1000)+'</td>' +
+                            '<td>'+formatNumber(item.current_price.avg_price*1000)+'</td>' +
+                            '<td>'+formatNumber(item.current_price.max_price*1000)+'</td></tr>'
                     )
                 }
             } else {
                 itemsTable.append(
                     '<tr><td><a href="/prices/'+encodeURIComponent(item.name)+'">'+item.name+'</a></td>'+
                     '<td>'+item.category+'</td>' +
-                    '<td>'+quantity+'</td>' +
-                    '<td>'+Math.round(item.current_price.min_price*quantity*1000)/1000+'</td>' +
-                    '<td>'+Math.round(item.current_price.avg_price*quantity*1000)/1000+'</td>' +
-                    '<td>'+Math.round(item.current_price.max_price*quantity*1000)/1000+'</td></tr>'
+                    '<td>'+formatNumber(quantity)+'</td>' +
+                    '<td>'+formatNumber(item.current_price.min_price*quantity)+'</td>' +
+                    '<td>'+formatNumber(item.current_price.avg_price*quantity)+'</td>' +
+                    '<td>'+formatNumber(item.current_price.max_price*quantity)+'</td></tr>'
                 )
             }
         });
